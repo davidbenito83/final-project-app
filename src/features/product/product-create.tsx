1ccb6088a07df0f3bd93d600d47af96e785e5f46
davidbenito83 <david.benito.romanillos@gmail.com>
2020-05-17 20:31:49 +0200

    Cambio de estilos

--- src/features/product/product-create.tsx
+++ src/features/product/product-create.tsx
@@ -24,24 +24,24 @@ export const ProductCreate: React.FunctionComponent<Props> = ({ onCreate, produc
 
   return (
     <>
-      <form action="/products/new" method="POST">
-        <label htmlFor="nombre">Nombre del producto</label>
+      <form action="/products/new" method="POST" className="form-type-post">
+        <label htmlFor="nombre">Nombre del producto</label><br />
         <input type="text" name="name" className="form-control" id="nombre" placeholder="Nombre"
-               value={productName} onChange={(event) => setproductName(event.target.value)} required></input>
-        <label htmlFor="cantidad">Cantidad que desea pujar</label>
+               value={productName} onChange={(event) => setproductName(event.target.value)} required></input><br />
+        <label htmlFor="cantidad">Cantidad que desea pujar</label><br />
         <input type="number" name="quantity" className="form-control" id="cantidad" placeholder="Cantidad"
-               value={quantity} onChange={(event) => setquantity(event.target.value)} required></input>
-        <label htmlFor="urlimagen">Imagen del producto</label>
+               value={quantity} onChange={(event) => setquantity(event.target.value)} required></input><br />
+        <label htmlFor="urlimagen">Imagen del producto</label><br />
         <input type="text" name="image" className="form-control" id="urlimagen" placeholder="URL de la imagen"
-               value={productImage} onChange={(event) => setproductImage(event.target.value)} required></input>
-        <label htmlFor="user">Usuario del producto</label>
+               value={productImage} onChange={(event) => setproductImage(event.target.value)} required></input><br />
+        <label htmlFor="user">Usuario del producto</label><br />
         <input type="text" name="userAssoc" className="form-control" id="user" placeholder="Usuario del producto"
-               value={userAssoc} onChange={(event) => setuserAssoc(event.target.value)} required></input>
-        <label htmlFor="descripcion">Descripción del producto</label>
+               value={userAssoc} onChange={(event) => setuserAssoc(event.target.value)} required></input><br />
+        <label htmlFor="descripcion">Descripción del producto</label><br />
         <textarea name="description" className="form-control" id="descripcion" placeholder="Descrición"
-               value={productDescription} onChange={(event) => setproductDescription(event.target.value)} required></textarea>
+               value={productDescription} onChange={(event) => setproductDescription(event.target.value)} required></textarea><br />
         <input type="hidden" name="state" className="form-control" id="validationDefault01"
-               value="true"></input>
+               value="true"></input><br />
         <Button submit>Crear Producto</Button>
       </form>
     </>
