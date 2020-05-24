756f6f13bd9d45c30416ee8b0b35572e01af6576
davidbenito83 <david.benito.romanillos@gmail.com>
2020-05-24 19:02:01 +0200

    Se setean las variables de entrada en usermodify

--- src/features/lists/userCard.tsx
+++ src/features/lists/userCard.tsx
@@ -30,12 +30,16 @@ export const UserCard: React.FunctionComponent<Props> = ({ user }) => {
 
   }
 
-  function userEdit(id: Object) {
+  // function userEdit(id: Object, name: string, email: string, password: string, role:string, state:boolean) {
 
-    if(id !== 'null' && typeof id != 'undefined'){
+  function userEdit(user: User) {
 
+    console.log(user.id);
+
+    if(typeof user.id != 'undefined'){
+
+      modifyUser(user.id,user.name,user.email,user.password,user.role,user.state);
       showModal();
-      console.log(id);
       // return(
       //   <Modal show={state.show} handleClose={hideModal}>
       //     <Button onClick={hideModal}><Icon>close</Icon></Button>
@@ -64,10 +68,12 @@ export const UserCard: React.FunctionComponent<Props> = ({ user }) => {
 
   const [updateUsers, setUpdateUsers] = useState<User[]>([])
 
-  async function modifyUser(id: Id, name: string, email: string, password: string, role:string, state:boolean ) {
+  async function modifyUser(id: Object, name: string, email: string, password: string, role:string, state:boolean ) {
     const updateUser: User = { id: user.id, name: user.name,  email: user.email, password: user.password, role: user.role, state: user.state}
     setUpdateUsers([...updateUsers, updateUser])
   }
+
+  const modalHeader = "modal-header";
 
   return (
     <>
@@ -77,12 +83,12 @@ export const UserCard: React.FunctionComponent<Props> = ({ user }) => {
         <td className="cell-table">{user.role}</td>
         <td className="cell-table">{user.state ? "SI" : "NO"}</td>
         <td className="cell-table"><Link to={"/user/" + user.email}><Icon>visibility</Icon></Link></td>
-        <td className="cell-table"><Button onClick={() => userEdit(user.id)}><Icon>edit</Icon></Button></td>
+        <td className="cell-table"><Button onClick={() => userEdit(user)}><Icon>edit</Icon></Button></td>
         <td className="cell-table"><Button onClick={() => userDelete(user.id)}><Icon>delete</Icon></Button></td>
       </tr>
       <tr>
         <Modal show={state.show} handleClose={hideModal}>
-          <div>
+          <div className={modalHeader}>
             <h3>Editando usuario <span>{user.name}</span></h3>
             <Button onClick={hideModal}><Icon>close</Icon></Button>
           </div>
--- src/features/user/user-modify.tsx
+++ src/features/user/user-modify.tsx
@@ -15,33 +15,74 @@ interface Props {
 }
 
 export const UserModify: React.FunctionComponent<Props> = ({ onCreate, users }) => {
-  const [userName, setuserName] = useState('')
-  const [userRole, setuserRole] = useState('')
-  const [userEmail, setuserEmail] = useState('')
-  const [userPassword, setuserPassword] = useState('')
+
+  let innerName = '';
+  let innerEmail = '';
+  let innerRole = '';
+  let innerPassword = '';
+  let innerId = 0;
+
+  if (typeof users !== 'undefined' && users.length > 0){
+    innerName = users[0].name;
+    innerEmail = users[0].email;
+    innerRole = users[0].role;
+    innerPassword = users[0].password;
+    innerId = users[0].id;
+  }
+
+  const [userName, setuserName] = useState<string>('')
+  const [userRole, setuserRole] = useState<string>('')
+  const [userEmail, setuserEmail] = useState<string>('')
+  const [userPassword, setuserPassword] = useState<string>('')
   const [userId, setuserId] = useState('')
 
-  console.log(users);
 
-  return (
-    <>
-      <form action="/users/update" method="POST" className="form-type-post">
-        <label htmlFor="nombre">Nombre del usuario</label><br />
-        <input type="text" name="name" className="form-control" id="nombre" placeholder="Nombre"
-               value={userName} disabled={true} onChange={(event) => setuserName(event.target.value)} required></input><br />
-        <label htmlFor="password">Password del usuario</label><br />
-        <input type="password" name="password" className="form-control" id="password" placeholder="Password"
-               value={userPassword} onChange={(event) => setuserPassword(event.target.value)} required></input><br />
-        <label htmlFor="email">Email del usuario</label><br />
-        <input type="text" name="email" className="form-control" id="email" placeholder="Email"
-               value={userEmail} onChange={(event) => setuserEmail(event.target.value)} required></input><br />
-        <label htmlFor="role">Rol de usuario</label><br />
-        <input name="role" className="form-control" id="role" placeholder="Role"
-               value={userRole} onChange={(event) => setuserRole(event.target.value)} required></input><br />
-        <input type="hidden" name="state" className="form-control" id="validationDefault01"
-               value="true"></input><br />
-        <Button submit>Modificar Usuario</Button>
-      </form>
-    </>
-  )
+  if (typeof users !== 'undefined' && users.length > 0){
+
+    return (
+      <>
+        <form action="/users/update" method="POST" className="form-type-post">
+          <label htmlFor="nombre">Nombre del usuario</label><br />
+          <input type="text" name="name" className="form-control" id="nombre" placeholder="Nombre"
+                 value={userName} onChange={(event) => setuserName(event.target.value)}
+                 required></input><br/>
+          <label htmlFor="password">Password del usuario</label><br/>
+          <input type="password" name="password" className="form-control" id="password" placeholder="Password"
+                 value={userPassword} onChange={(event) => setuserPassword(event.target.value)}
+                 required></input><br/>
+          <label htmlFor="email">Email del usuario</label><br/>
+          <input type="text" name="email" className="form-control" id="email" placeholder="Email"
+                 value={userEmail} onChange={(event) => setuserEmail(event.target.value)} required></input><br/>
+          <label htmlFor="role">Rol de usuario</label><br/>
+          <input name="role" className="form-control" id="role" placeholder="Role"
+                 value={userRole} onChange={(event) => setuserRole(event.target.value)} required></input><br/>
+          <input type="hidden" name="id" className="form-control"
+                 value={users[0].id}></input>
+          <input type="hidden" name="state" className="form-control"
+                 value="true"></input><br/>
+          <Button submit>Modificar Usuario</Button>
+        </form>
+      </>
+    )
+
+  } else {
+    return (
+      <>
+        <div>
+          <h3>
+            error
+          </h3>
+        </div>
+      </>
+    )
+  }
+
+
+
+  // setuserName(users[0].name);
+  // setuserRole(users[0].role);
+  // setuserEmail(users[0].email);
+  // setuserPassword(users[0].password);
+
+
 }
